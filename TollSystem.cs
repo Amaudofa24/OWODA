0b3839391a524c81c55f05c9549fc8be7b462c90
Support <Support@WEMA-TRAINING-S>
2022-08-11 21:08:16 +0100

    Add project files.

---
+++ TollSystem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    internal class AccountOnboarding
+    {
+        public string CustomerName { get; set; }
+
+        public void CreateAccount()
+        {
+
+        }
+
+        public string SendMail()
+        {
+            return "Email Sent";
+        }
+
+        public void CommitCustomerCare()
+        {
+
+        }
+
+    }
+
+    internal class CustomerAudit
+    {
+        void SendQuestionnaires()
+        {
+            //codes
+            //codes
+            var comm = new Communications();
+            comm.SendMail();
+            //codes
+        }
+    }
+
+   public class Communications
+    {
+        public string SendMail()
+        {
+            return "Email Sent";
+        }
+
+        public string SendSMS()
+        {
+            return "SMS Sent";
+        }
+
+    }
+}
